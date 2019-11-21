0d08b4fe55459b91e182efd0ccebbc3237def545
Maria Luque Anguita <mluqueanguita@gmail.com>
2019-11-21 14:03:26 +0000

    best: 214.996

--- src/ea/Parameters.java
+++ src/ea/Parameters.java
@@ -2,28 +2,15 @@ package ea;
 
 import java.util.Random;
 
-public class Parameters {
-
+public class Parameters
+{
 	public static Random rnd = new Random(System.currentTimeMillis());
-	
-	/**
-	 * used as a seed
-	 * 
-	 */	
 	static final boolean [] DEFAULT_WOMENS_TRANSITION_STRATEGY = {true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true};
 	public static final int [] DEFAULT_WOMENS_PACING_STRATEGY = {300, 300, 300, 300, 300, 300, 300, 350, 350, 300, 300, 350, 350, 350, 350, 300, 300, 350, 350, 350, 350, 300, 300};
-	
-	
-	public static int popSize = 70; // population size - initially 20
+	public static int popSize = 100; // population size - initially 20 - 70
 	public static int tournamentSize = 10; // selection process - initially 2
-
-	// big tournament high: reduces the chance of picking low fitness individuals
-
 	public static int mutationRateMax = 6; // out of len - initially 6, can be up to 23
 	public static double mutationProbability = 0.5; // initially 0.5
 	public static double crossoverProbability = 1.0; //initially 1.0
-	
-	public static int maxIterations = 1000; // initially 1000
-	
-	
+	public static int maxIterations = 10000; // initially 1000
 }
