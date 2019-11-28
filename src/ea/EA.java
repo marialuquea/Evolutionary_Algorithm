e30fc3bba6e74a4d1ccce96d89f941c57271212d
Maria Luque Anguita <mluqueanguita@gmail.com>
2019-11-28 10:54:03 +0000

    some updates

--- src/ea/EA.java
+++ src/ea/EA.java
@@ -30,11 +30,6 @@ public class EA
 	
 	public EA() { }
 
-	public Individual bestI(){
-		Individual best = getBest(population);
-		return best;
-	}
-	
 	public static void main(String[] args) {
 		EA ea = new EA();
 		// ea.run();
@@ -72,93 +67,21 @@ public class EA
 		System.out.println("" + iteration + "\t best:" + getBest(population) + "\t \t worst:" + getWorst(population) + "\t pop size: " + population.size());
 	}
 
-	public void removeIndividual(){
-		population.remove(getWorst(population));
-	}
-
-	private void refillPopulation(int start, int bound) {
-		// refill population with random seeds
+	// INITIALISE
+	private void initialisePopulation(int start, int bound) {
+		population.clear();
 		while(population.size() < Parameters.popSize){
 			Individual individual = new Individual();
 			individual.initialise(start, bound);
 			individual.evaluate(teamPursuit);
 			population.add(individual);
+
 		}
 	}
 
-	private void replace(Individual child) {
-		Individual worst = getWorst(population);
-		if(child.getFitness() < worst.getFitness()){
-			int idx = population.indexOf(worst);
-			population.set(idx, child);
-		}
-	}
 
-	private Individual hill_climber(Individual i){
-
-		System.out.println("\n---HILL CLIMBER---");
-		Individual best = i.copy();
-		Individual temp = i.copy(); // the best seed after running the EA
-		double before = temp.getFitness();
-
-		for (int j = 0; j < 2000; j++)
-		{
-			// flip one random transition gene
-			int index_transition = Parameters.rnd.nextInt(temp.transitionStrategy.length);
-			temp.transitionStrategy[index_transition] = !temp.transitionStrategy[index_transition];
-
-			// change the value of 1 pacing value
-			int mutationChange = 200 - Parameters.rnd.nextInt(100);
-			int index_pacing = Parameters.rnd.nextInt(temp.pacingStrategy.length);
-
-			// ensure mutation is valid
-			if (temp.pacingStrategy[index_pacing] + mutationChange >= 200 && temp.pacingStrategy[index_pacing] + mutationChange <= 1200)
-				temp.pacingStrategy[index_pacing] = temp.pacingStrategy[index_pacing] + mutationChange;
-
-			temp.evaluate(EA.teamPursuit);
-			//System.out.println("f: "+temp.getFitness());
-
-			if (temp.getFitness() <  best.getFitness()) {
-				best = temp.copy();
-				System.out.println("better: "+best.getFitness()+" at iteration "+j);
-			}
-			else
-				temp = best.copy();
-		}
-		System.out.println("best: "+best.getFitness());
-		return best;
-	}
-
-	// mutate and swamp mutation
-	// swaps 2 elements but doesn't introduce new genes so less exploration and limits the scope of the search space in which the algorithm can explore
-
-	private Individual mutate(Individual child) {
-		// mutation probability
-		if(Parameters.rnd.nextDouble() > Parameters.mutationProbability)
-			return child;
-
-		// mutate 1 to 6 genes
-		int mutationRate = 1 + Parameters.rnd.nextInt(Parameters.mutationRateMax);
-
-		for(int i = 0; i < mutationRate; i++)
-		{
-			// inverts a random transition from true to false and vice versa
-			int index_transition = Parameters.rnd.nextInt(child.transitionStrategy.length);
-			child.transitionStrategy[index_transition] = !child.transitionStrategy[index_transition];
-
-			// the bigger the mutation, the more diversity
-			int mutationChange = 200 - Parameters.rnd.nextInt(100);
-			int index_pacing = Parameters.rnd.nextInt(child.pacingStrategy.length);
-
-			// ensure mutation is valid
-			if (child.pacingStrategy[index_pacing] + mutationChange >= 200 && child.pacingStrategy[index_pacing] + mutationChange <= 1200)
-				child.pacingStrategy[index_pacing] = child.pacingStrategy[index_pacing] + mutationChange;
-		}
-		return child;
-	}
-
+	// SELECTION METHODS
 	private Individual RoulletteSelection() {
-		// SELECTION METHOD
 		// the lower the race time, the higher the fitness
 		Individual parent = new Individual(); // new empty parent
 
@@ -172,7 +95,7 @@ public class EA
 		}
 
 		// spinner - percentage of circle (0-1) * area of circle gives a position in the circle
-		double spinner = ThreadLocalRandom.current().nextDouble(0, 1) * total; // create a random number between 0 and 1
+		double spinner = total * ThreadLocalRandom.current().nextDouble(0, 1); // create a random number between 0 and 1
 
 		double count = 0; // go through all the circle until we reach the spinner.
 		for(Individual i : population) {
@@ -181,11 +104,9 @@ public class EA
 				return i;
 		}
 		return parent;
-	} // UNDERSTAND
+	}
 
-	/*
-	private Individual RankSelection()
-	{
+	private Individual RankSelection() {
 		Individual parent = new Individual(); // new empty parent
 
 		double scalingFactor = ThreadLocalRandom.current().nextDouble(0, 1) + 1;
@@ -201,15 +122,22 @@ public class EA
 			for (int j = 0; j < rank.size(); j++)
 			{
 				if (i.getFitness() > rank.get(j).getFitness())
-					rank.put
+					rank.add(i);
 			}
 		}
-
 		return parent;
 	}
 
-	 */
+	private Individual tournamentSelection() {
+		ArrayList<Individual> candidates = new ArrayList<Individual>();
+		for(int i = 0; i < Parameters.tournamentSize; i++){ // choose a number of chromosomes to participate in the tournament
+			candidates.add(population.get(Parameters.rnd.nextInt(population.size()))); // add random pop to array
+		}
+		return getBest(candidates).copy(); // return best one from array
+	}
 
+
+	// CROSSOVER METHODS
 	private Individual UniformCrossover(Individual parent1, Individual parent2) {
 		if(Parameters.rnd.nextDouble() > Parameters.crossoverProbability)
 			return parent1;
@@ -237,7 +165,6 @@ public class EA
 		return child1;
 	}
 
-	// partially mapped crossover
 	private Individual multipoint(Individual parent1, Individual parent2) {
 		// crossover probability
 		if(Parameters.rnd.nextDouble() > Parameters.crossoverProbability)
@@ -316,30 +243,66 @@ public class EA
 		}
 
 		return child;
+	}	private Individual getBest(ArrayList<Individual> aPopulation) {
+	double bestFitness = Double.MAX_VALUE;
+	Individual best = null;
+	for(Individual individual : aPopulation){
+		if(individual.getFitness() < bestFitness || best == null){
+			best = individual;
+			bestFitness = best.getFitness();
+		}
+	}
+	return best;
+}
+
+
+	// MUTATION METHODS
+	private Individual mutate(Individual child) {
+		// mutation probability
+		if(Parameters.rnd.nextDouble() > Parameters.mutationProbability)
+			return child;
+
+		// mutate 1 to 6 genes
+		int mutationRate = 1 + Parameters.rnd.nextInt(Parameters.mutationRateMax);
+
+		for(int i = 0; i < mutationRate; i++)
+		{
+			// inverts a random transition from true to false and vice versa
+			int index_transition = Parameters.rnd.nextInt(child.transitionStrategy.length);
+			child.transitionStrategy[index_transition] = !child.transitionStrategy[index_transition];
+
+			// the bigger the mutation, the more diversity
+			int mutationChange = 200 - Parameters.rnd.nextInt(100);
+			int index_pacing = Parameters.rnd.nextInt(child.pacingStrategy.length);
+
+			// ensure mutation is valid
+			if (child.pacingStrategy[index_pacing] + mutationChange >= 200 && child.pacingStrategy[index_pacing] + mutationChange <= 1200)
+				child.pacingStrategy[index_pacing] = child.pacingStrategy[index_pacing] + mutationChange;
+		}
+		return child;
+	}
+	// mutate by swap mutation
+	// swaps 2 elements but doesn't introduce new genes so less exploration and limits the scope of the search space in which the algorithm can explore
+	private Individual swapMutation(Individual child){
+		// mutation probability
+		if(Parameters.rnd.nextDouble() > Parameters.mutationProbability)
+			return child;
+
+
 	}
 
-	/**
-	 * Returns a COPY of the individual selected using tournament selection
-	 * @return
-	 */
-	private Individual tournamentSelection() {
-		ArrayList<Individual> candidates = new ArrayList<Individual>();
-		for(int i = 0; i < Parameters.tournamentSize; i++){ // choose a number of chromosomes to participate in the tournament
-			candidates.add(population.get(Parameters.rnd.nextInt(population.size()))); // add random pop to array
-		}
-		return getBest(candidates).copy(); // return best one from array
-	}
-
-	private Individual getBest(ArrayList<Individual> aPopulation) {
-		double bestFitness = Double.MAX_VALUE;
-		Individual best = null;
-		for(Individual individual : aPopulation){
-			if(individual.getFitness() < bestFitness || best == null){
-				best = individual;
-				bestFitness = best.getFitness();
-			}
-		}
+	// OTHER
+	public Individual bestI(){
+		Individual best = getBest(population);
 		return best;
+	}
+
+	private void replace(Individual child) {
+		Individual worst = getWorst(population);
+		if(child.getFitness() < worst.getFitness()){
+			int idx = population.indexOf(worst);
+			population.set(idx, child);
+		}
 	}
 
 	private Individual getWorst(ArrayList<Individual> aPopulation) {
@@ -353,21 +316,64 @@ public class EA
 		}
 		return worst;
 	}
-	
+
 	private void printPopulation() {
 		for(Individual individual : population){
 			System.out.println(individual);
 		}
 	}
 
-	private void initialisePopulation(int start, int bound) {
-		population.clear();
+
+	// DIVERSITY
+	public void removeIndividual(){
+		population.remove(getWorst(population));
+	}
+
+	private void refillPopulation(int start, int bound) {
+		// refill population with random seeds
 		while(population.size() < Parameters.popSize){
 			Individual individual = new Individual();
 			individual.initialise(start, bound);
 			individual.evaluate(teamPursuit);
 			population.add(individual);
-							
-		}		
-	}	
+		}
+	}
+
+	private Individual hill_climber(Individual i){
+
+		System.out.println("\n---HILL CLIMBER---");
+		Individual best = i.copy();
+		Individual temp = i.copy(); // the best seed after running the EA
+		double before = temp.getFitness();
+
+		for (int j = 0; j < 2000; j++)
+		{
+			// flip one random transition gene
+			int index_transition = Parameters.rnd.nextInt(temp.transitionStrategy.length);
+			temp.transitionStrategy[index_transition] = !temp.transitionStrategy[index_transition];
+
+			// change the value of 1 pacing value
+			int mutationChange = 200 - Parameters.rnd.nextInt(100);
+			int index_pacing = Parameters.rnd.nextInt(temp.pacingStrategy.length);
+
+			// ensure mutation is valid
+			if (temp.pacingStrategy[index_pacing] + mutationChange >= 200 && temp.pacingStrategy[index_pacing] + mutationChange <= 1200)
+				temp.pacingStrategy[index_pacing] = temp.pacingStrategy[index_pacing] + mutationChange;
+
+			temp.evaluate(EA.teamPursuit);
+			//System.out.println("f: "+temp.getFitness());
+
+			if (temp.getFitness() <  best.getFitness()) {
+				best = temp.copy();
+				System.out.println("better: "+best.getFitness()+" at iteration "+j);
+			}
+			else
+				temp = best.copy();
+		}
+		System.out.println("best: "+best.getFitness());
+		return best;
+	}
+
+
+
 }
